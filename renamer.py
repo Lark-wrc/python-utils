b4178d925390b133187046de3beffe0c5841645d
Bill Clark <clarkw26@students.rowan.edu>
2014-10-18 21:30:59 -0400

    new recursive upgrade to renamer.

--- renamer.py
+++ renamer.py
@@ -4,9 +4,7 @@ checker = True
 
 """	Takes in the arguements. First being the directory in question,
 	the secound, if exists, can be -q to turn off confirmation.
-
-	old version.
-
+"""
 if len(sys.argv) == 2:
 	os.chdir(sys.argv[1])
 elif len(sys.argv) == 3:
@@ -14,76 +12,69 @@ elif len(sys.argv) == 3:
 	if sys.argv[2] == '-q':
 		checker = False
 else:
-	print '[-q] directory'
+	print 'false'
 	exit()
-"""
 
-dirs = []
-for x in sys.argv[1:]:
-	if x[0] == '-':
-		arg = x[1:]
-		if arg == 'q':
-			checker = False
-	else:
-		dirs.append(x)
+#List out all of the files.
+files = os.listdir('.')
 
-#No directories found.
-if len(dirs) == 0:
-	print '[-q] directories'
+#For each file, split it into the name and the extension, b and c.
+#All directories are skipped.
+for f in files:
+	if os.path.isdir(f):
+		continue
+	out = []
+	inpareth = False
+	b = f[0:len(f)-4]
+	c = f[len(f)-4:]
 
+	""" 
+	For every character in the filename, either replace '_' or 
+	'.' with a ' ' character, do nothing with regular characters, or
+	filter out every character between any type of paretheses.
+	"""
+	for i in b:
+		if not inpareth:
+			if i == '_' or i == '.':
+				out.append(" ")
+				continue
+			elif i == '[' or i == '(' or i == '{':
+				inpareth = True
+			else:
+				out.append(i)
+		elif inpareth:
+			if i == ']' or i == ')' or i == '}':
+				inpareth = False
 
-startdir = os.getcwd()
-for cdir in dirs:
-	os.chdir(cdir)
-	
-	#List out all of the files.
-	files = os.listdir('.')
+	#Join the new file name, and remove a leading '+' if neccessary.
+	b = ''.join(out)
+	b = b.split()
+	d = []
+	if b[0][0] == '+':
+		b[0] = b[0][1:]
 
-	#For each file, split it into the name and the extension, b and c.
-	#All directories are skipped.
-	for f in files:
-		if os.path.isdir(f):
-			continue
-		out = []
-		inpareth = False
-		fname = f[0:len(f)-4]
-		ext = f[len(f)-4:]
-
-		""" 
-		For every character in the filename, either replace '_' or 
-		'.' with a ' ' character, do nothing with regular characters, or
-		filter out every character between any type of paretheses.
-		"""
-		for i in fname:
-			if not inpareth:
-				if i == '_' or i == '.':
-					out.append(" ")
-					continue
-				elif i == '[' or i == '(' or i == '{':
-					inpareth = True
-				else:
-					out.append(i)
-			elif inpareth:
-				if i == ']' or i == ')' or i == '}':
-					inpareth = False
-
-		#Update the filename to the changed one.
-		fname = out
-		if fname[0] == '+':
-			fname = fname[1:]
-
-		#combine the filename into a string and attach it's extension.
-		out = ''.join(fname)
-		out = out.strip() + ext
-
-		#print the file name, and ask for confirmation before renaming. 
-		print out
-		if checker:
-			a = raw_input("this okay? y/n ")
-			if a=='y' or a=='yes' or a=='':
-				os.rename(f, out)
+	#removed, for appending a 1x to the episode number.
+	"""
+	for i in b:
+		if i.isdigit():
+			#d.append('1x' + i)
+			d.append(i)
 		else:
+			d.append(i)
+		d.append(' ')
+	"""
+
+	#combine the filename into a string and attach it's extension.
+	out = ' '.join(b)
+	out = out.strip() + c
+
+	#print the file name, and ask for confirmation before renaming. 
+	print out
+	if checker:
+		a = raw_input("this okay? y/n ")
+		if a=='y' or a=='yes' or a=='':
 			os.rename(f, out)
-	os.chdir(startdir)
+	else:
+		os.rename(f, out)
 	
 print 'Sayonara.'
---
+++ subfixer.py
@@ -0,0 +1,69 @@
+import os, sys
+
+def fix(dir='.', check=1, recur=1, chk = 1):
+	userconfirm = check
+	recurse = recur
+	dirchk = chk
+
+	if len(sys.argv) > 1:
+		for x in sys.argv[1:]:
+			if x == '--check' or x == '-c':
+				userconfirm = 0
+			elif x == '--help' or x == '-h':
+				print 'Recusively renames all files in the directory listed.'
+				print 'Commands: '
+				print   """-c	--check		Disable filename checker.
+					-n	--nocur		Disables recurance.
+					-d	--nodirchk	Disables recursion confirmation. RISKY.
+					-h	--help		This should be obvious.	"""
+			elif x == '--norecur' or x == '-n':
+				recur = 0
+			elif x == '--nodirchk' or x == '-d':
+				dirchk = 0
+			else:
+				os.chdir(x)
+		sys.argv = []
+	else:
+		os.chdir(dir)
+	
+	
+	list = os.listdir('.')
+	
+	for f in files:
+		if os.path.isdir(f):
+			a = raw_input(f + ' recurse? ')
+			if dirchk and (a == 'y' or a == 'yes' or a == ''):
+				fix(f, userconfirm, recurse, dirchk)
+			continue
+		output = []
+		clearing = 0
+		
+		filename = f[0:-4]
+		extent = f[-4:]
+		for i in filename:
+			if not clearing:
+				if i == '_' or i == '.':
+					output.append(' ')
+				elif i == '[' or i == '(' or i == '{':
+					clearing = 1
+				else
+					output.append(i)
+			else:
+				if i == ']' or i == ')' or i == '}':
+					clearing = 0
+		if output[0] == '+':
+			output = output[1:]
+		output = ' '.join(output)
+		output = output.strip() + extent
+		
+		print output
+		if userconfirm:
+			a = raw_input("Okay? y/n ")
+			if a=='y' or a=='yes' or a=='':
+				os.rename(f, output)
+			else:
+				os.rename(f, output)
+	print 'Folder Complete~'
+if if __name__ == "__main__":
+	fix()
+	print 'Mission Complete~'
