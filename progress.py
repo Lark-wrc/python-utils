c0f6324058fcf30db86ce81033778f924754d020
Bill Clark <billc@indellient.com>
2017-10-27 10:22:47 -0400

    Added the CounterForm decorater that show progress and notes each time a specific number of animates plays.

--- progress.py
+++ progress.py
@@ -5,16 +5,31 @@ class Spinner():
     
     def __init__(self):
         self.FRAMES = ['\\', '|', '/', '-']
+    
+    def _write(self, content, tail=''):
+        stdout.write('\r['+content+']'+tail)
+        stdout.flush()
 
     def animate(self):
         self.FRAMES = self.FRAMES[1:]+self.FRAMES[:1]
-        stdout.write('\r['+self.FRAMES[0]+']')
-        stdout.flush()
+        self._write(self.FRAMES[0])
+
+class CounterForm(Spinner):
+    def __init__(self, decorated, count=25):
+        self.decorated = decorated
+        self.ticks = 0
+        self.count = 25
+    
+    def animate(self):
+        self.decorated.animate()
+        self.ticks+=1
+        if self.ticks%self.count == 0:
+            self._write(str(self.ticks), '\n')
 
 if __name__ == "__main__":
     from time import sleep
     
-    s = Spinner()
+    s = CounterForm(Spinner())
     while 1:
         s.animate()
-        sleep(.2)
+        sleep(.5)
